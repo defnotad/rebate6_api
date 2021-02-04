39321a2df28bacab3f5e66818bdf3fd7814636c6
defnotad <aditya.arya.ggn@gmail.com>
2021-02-04 17:17:55 +0530

    Post method ready

--- index.js
+++ index.js
@@ -45,7 +45,47 @@ app.get('/user', async function (req, res) {
     });
 });
 
-app.post('/user', async function (req, res) {
+
+app.post('/transaction', async function (req, res) {
     const phone = req.query.phone;
+    const merchantID = 'Btg9U5yCb1b3dQpqNaEC';
+    const originalAmount = req.query.originalAmount;
+    const pointsUsed = req.query.pointsUsed;
+    const discountGiven = req.query.discountGiven;
+    // const dateTime = Date();
+    await onlineDB.doc(merchantID).get().then(async (merchant) => {
+        console.log("Works 1");
+        if (merchant.exists) {
+            await admin.auth().getUserByPhoneNumber('+91' + phone).then(async function (user) {
+                console.log("Works 2");
+                await usersDB.doc(user.uid).get().then(async details => {
+                    console.log("Works 3");
+                    const oldPoints = details.data()['points'];
+                    if (pointsUsed > oldPoints) {
+                        res.send("Not enough points");
+                    } else {
+                        console.log("Works 4");
+                        const newPoints = oldPoints - pointsUsed;
+                        await usersDB.doc(user.uid).update({
+                            points: newPoints,
+                            online_transactions: {
+                                originalAmount: Number(originalAmount),
+                                pointsUsed: Number(pointsUsed),
+                                discountGiven: Number(discountGiven),
+                                // dateTime: dateTime,
+                                merchantID: merchantID
+                            },
+                        }).then(async () => {
+                            res.send("Yes");
+                        }).catch((err) => console.log(err));
+                    }
+                });
+            }).catch(() => {
+                res.send("Sorry");
+            });
+        } else {
+            res.send("Invalid source");
+        }
+    });
     // await 
 });
